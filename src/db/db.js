abaa7bf31fed1ba70b0adaca72762567a66b384e
RUDHRAM SARASWAT <90370450+RUDHRAM01@users.noreply.github.com>
2023-03-19 19:34:45 +0530

    update

--- src/db/db.js
+++ src/db/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://rudhram:rudhram@cluster0.4xvlplx.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { console.log('Database connected successfully ') }).catch((err) => { console.log('Error connecting to database ' + err) });
+mongoose.connect("dbconnectionlink", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { console.log('Database connected successfully ') }).catch((err) => { console.log('Error connecting to database ' + err) });
